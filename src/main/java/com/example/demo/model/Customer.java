0aefa8e3e248e1a2e240acfa8e045396600b06d7
Clarks1223 <pablo.uchuari@epn.edu.ec>
2024-08-14 10:22:37 -0500

    Optimizacion del model Customer con la libreria Lombok

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,6 +1,11 @@
 package com.example.demo.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
+@Data//genera getters y setters de todos los atributos de la clase
+@AllArgsConstructor//genera un constructor con todos los atributos de la clase
 public class Customer {
     //atributos
     @NonNull
@@ -13,52 +18,4 @@ public class Customer {
     private String password;
     @NonNull
     private String email;
-
-    public Customer(int id, String name, String username, String password, String email) {
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
